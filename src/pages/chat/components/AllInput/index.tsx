d01e9b536b50b1211cbb9a3c89625c29419d4724
79E <5980844@qq.com>
2023-05-06 09:07:20 +0800

    fix: 修改角色文案选中后操作

--- src/pages/chat/components/AllInput/index.tsx
+++ src/pages/chat/components/AllInput/index.tsx
@@ -17,13 +17,12 @@ function AllInput(props: Props) {
   const [prompt, setPrompt] = useState('')
   const { localPrompt } = useStore()
 
-  const bodyResize = useDocumentResize();
+  const bodyResize = useDocumentResize()
 
   const [downloadModal, setDownloadModal] = useState({
     open: false,
     loading: false
   })
-
 
   const searchOptions = useMemo(() => {
     if (prompt.startsWith('/')) {
@@ -45,48 +44,43 @@ function AllInput(props: Props) {
   // 保存聊天记录到图片
   async function downloadChatRecords() {
     try {
-      setDownloadModal(d => ({ ...d, loading: true }));
+      setDownloadModal((d) => ({ ...d, loading: true }))
       const ele = document.getElementById('image-wrapper')
       const canvas = await html2canvas(ele as HTMLDivElement, {
-        useCORS: true,
+        useCORS: true
       })
       const imgUrl = canvas.toDataURL('image/png')
       const tempLink = document.createElement('a')
       tempLink.style.display = 'none'
       tempLink.href = imgUrl
       tempLink.setAttribute('download', 'chat-shot.png')
-      if (typeof tempLink.download === 'undefined')
-        tempLink.setAttribute('target', '_blank')
+      if (typeof tempLink.download === 'undefined') tempLink.setAttribute('target', '_blank')
       document.body.appendChild(tempLink)
       tempLink.click()
       document.body.removeChild(tempLink)
       window.URL.revokeObjectURL(imgUrl)
-      setDownloadModal(() => ({ open: false, loading: false }));
+      setDownloadModal(() => ({ open: false, loading: false }))
       Promise.resolve()
-    }
-    catch (error: any) {
+    } catch (error: any) {
       message.error('下载聊天记录失败')
       Promise.reject()
-    }
-    finally {
-      setDownloadModal(d => ({ ...d, loading: false }));
+    } finally {
+      setDownloadModal((d) => ({ ...d, loading: false }))
     }
   }
 
   return (
     <div className={styles.allInput}>
-      {
-        bodyResize.width > 800 && (
-          <div
-            className={styles.allInput_icon}
-            onClick={() => {
-              setDownloadModal(d => ({ ...d, open: true }));
-            }}
-          >
-            <CloudDownloadOutlined />
-          </div>
-        )
-      }
+      {bodyResize.width > 800 && (
+        <div
+          className={styles.allInput_icon}
+          onClick={() => {
+            setDownloadModal((d) => ({ ...d, open: true }))
+          }}
+        >
+          <CloudDownloadOutlined />
+        </div>
+      )}
       <div
         className={styles.allInput_icon}
         onClick={() => {
@@ -104,9 +98,10 @@ function AllInput(props: Props) {
         }}
         onSelect={(value) => {
           // 这里选择后直接发送
-          props?.onSend?.(value)
+          //   props?.onSend?.(value)
           // 并且将输入框清空
-          setPrompt('')
+          // 修改为选中放置在输入框内
+          setPrompt(value)
         }}
       >
         <Input.TextArea
@@ -134,44 +129,40 @@ function AllInput(props: Props) {
           }}
         />
       </AutoComplete>
-      {
-
-        props.disabled ? (
-          <Button
-            className={styles.allInput_button}
-            type="primary"
-            size="large"
-            ghost
-            danger
-            disabled={!props.disabled}
-            onClick={() => {
-              props.onStopFetch?.()
-            }}
-          >
-            <SyncOutlined spin /> 停止回答 🤚
-          </Button>
-        ) : (
-          <Button
-            className={styles.allInput_button}
-            type="primary"
-            size="large"
-            disabled={!prompt || props.disabled}
-            onClick={() => {
-              props?.onSend?.(prompt)
-              setPrompt('')
-            }}
-          >
-            发送
-          </Button>
-        )}
-
-
+      {props.disabled ? (
+        <Button
+          className={styles.allInput_button}
+          type="primary"
+          size="large"
+          ghost
+          danger
+          disabled={!props.disabled}
+          onClick={() => {
+            props.onStopFetch?.()
+          }}
+        >
+          <SyncOutlined spin /> 停止回答 🤚
+        </Button>
+      ) : (
+        <Button
+          className={styles.allInput_button}
+          type="primary"
+          size="large"
+          disabled={!prompt || props.disabled}
+          onClick={() => {
+            props?.onSend?.(prompt)
+            setPrompt('')
+          }}
+        >
+          发送
+        </Button>
+      )}
 
       <Modal
         title="保存当前对话记录"
         open={downloadModal.open}
         onOk={() => {
-          downloadChatRecords();
+          downloadChatRecords()
         }}
         confirmLoading={downloadModal.loading}
         onCancel={() => {
