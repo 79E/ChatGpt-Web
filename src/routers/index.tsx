fb1cdcf23ce9164114743dca38c8e6d818a3549e
79E <5980844@qq.com>
2023-05-06 16:30:36 +0800

    feat: 新增渲染路由懒加载状态

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,72 +1,67 @@
-import type { RouteObject } from 'react-router-dom';
+import React from 'react'
+import type { RouteObject } from 'react-router-dom'
 
 type ConfigureType = {
-    verifToken?: boolean,
-    title?: string
+  verifToken?: boolean
+  title?: string
 }
 
-export interface RouteOptions extends Omit<Omit<RouteObject, 'children'>,'index'> {
-    index?: boolean;
-    children?: RouteOptions[];
-    configure?: ConfigureType;
+export interface RouteOptions extends Omit<Omit<RouteObject, 'children'>, 'index'> {
+  index?: boolean
+  children?: RouteOptions[]
+  configure?: ConfigureType
 }
 
-import ChatPage from '@/pages/chat';
-import DrawPage from '@/pages/draw';
+const ChatPage = React.lazy(() => import('@/pages/chat'))
+const DrawPage = React.lazy(() => import('@/pages/draw'))
 
 const routes: RouteOptions[] = [
-    {
-        id: 'ChatPage',
-        path: '/',
-        element: <ChatPage />,
-        children:[],
-        configure: {
-            verifToken: false
-        },
-    },
-    {
-        id: 'DrawPage',
-        path: '/draw',
-        element: <DrawPage />,
-        children:[],
-        configure: {
-            verifToken: false
-        },
-    },
-    {
-        id: 'ShopPage',
-        path: '/shop',
-        element: <ChatPage />,
-        children:[],
-        configure: {
-            verifToken: true
-        },
+  {
+    id: 'ChatPage',
+    path: '/',
+    element: <ChatPage />,
+    children: [],
+    configure: {
+      verifToken: false
     }
-];
+  },
+  {
+    id: 'DrawPage',
+    path: '/draw',
+    element: <DrawPage />,
+    children: [],
+    configure: {
+      verifToken: false
+    }
+  },
+  {
+    id: 'ShopPage',
+    path: '/shop',
+    element: <ChatPage />,
+    children: [],
+    configure: {
+      verifToken: true
+    }
+  }
+]
 
-export function searchRouteDetail(
-    path: string,
-    routes: RouteOptions[]
-): RouteOptions | null {
-    let detail = null;
+export function searchRouteDetail(path: string, routes: RouteOptions[]): RouteOptions | null {
+  let detail = null
 
-    const forRouter = (
-        path: string,
-        routes: RouteOptions[]
-    )=>{
-        for (const item of routes) {
-            if (item.path === path) {
-                detail = item;
-            }
-            if (item.children) {
-                forRouter(path, item.children);
-            }
-        }
-    };
+  const forRouter = (path: string, routes: RouteOptions[]) => {
+    for (const item of routes) {
+      if (item.path === path) {
+        detail = item
+      }
+      if (item.children) {
+        forRouter(path, item.children)
+      }
+    }
+  }
 
-    forRouter(path,routes);
+  forRouter(path, routes)
 
-    return detail;
+  return detail
 }
 
-export default routes as RouteObject[];
+export default routes as RouteObject[]
