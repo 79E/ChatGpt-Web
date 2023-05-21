0b7e7c728760e241ea913d34310f78e26b0a95dd
79E <5980844@qq.com>
2023-05-21 15:54:23 +0800

    feat: 新增携带历史信息数量配置

--- src/pages/admin/config/index.tsx
+++ src/pages/admin/config/index.tsx
@@ -2,7 +2,7 @@ import {
     ProFormDigit,
     QueryFilter,
 } from '@ant-design/pro-components';
-import { Form, message } from 'antd';
+import { Form, Space, message } from 'antd';
 import { useEffect, useState } from 'react';
 import styles from './index.module.less'
 import { getAdminConfig, putAdminConfig } from '@/request/adminApi';
@@ -16,24 +16,32 @@ function ConfigPage() {
         signin_reward: number | string,
     }>();
 
-    function getConfigValue (key: string, data: Array<ConfigInfo>){
+    const [historyMessageForm] = Form.useForm<{
+        history_message_count: number | string,
+    }>();
+
+    function getConfigValue(key: string, data: Array<ConfigInfo>) {
         const value = data.filter(c => c.name === key)[0]
         return value
     }
 
-    function onRewardFormSet (data: Array<ConfigInfo>){
+    function onRewardFormSet(data: Array<ConfigInfo>) {
         const registerRewardInfo = getConfigValue('register_reward', data)
         const signinRewardInfo = getConfigValue('signin_reward', data)
+        const historyMessageCountInfo = getConfigValue('history_message_count', data)
         rewardForm.setFieldsValue({
             register_reward: registerRewardInfo.value,
             signin_reward: signinRewardInfo.value
         })
+        historyMessageForm.setFieldsValue({
+            history_message_count: Number(historyMessageCountInfo.value)
+        })
     }
 
 
-    function onGetConfig (){
-        getAdminConfig().then((res)=>{
-            if(res.code){
+    function onGetConfig() {
+        getAdminConfig().then((res) => {
+            if (res.code) {
                 message.error('获取配置错误')
                 return
             }
@@ -42,53 +50,94 @@ function ConfigPage() {
         })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         onGetConfig()
-    },[])
+    }, [])
+
+    async function onSave(values: any){
+        return putAdminConfig(values).then((res) => {
+            if (res.code) {
+                message.error('保存失败')
+                return
+            }
+            message.success('保存成功')
+            onGetConfig()
+        })
+    }
 
     return (
         <div className={styles.config}>
-            <div className={styles.config_form}>
-                <h3>奖励激励</h3>
-                <QueryFilter
-                    form={rewardForm}
-                    onFinish={async (values: any) => {
-                        putAdminConfig(values).then((res)=>{
-                            if(res.code) {
-                                message.error('保存失败')
-                                return
-                            }
-                            message.success('保存成功')
-                            onGetConfig()
-                        })
-                    }}
-                    onReset={()=>{
-                        onRewardFormSet(configs)
-                    }}
-                    size="large"
-                    collapsed={false}
-                    defaultCollapsed={false}
-                    requiredMark={false}
-                    defaultColsNumber={79}
-                    searchText="保存"
-                    resetText="恢复"
-                >
-                    <ProFormDigit
-                        name="register_reward"
-                        label="注册奖励"
-                        tooltip="新用户注册赠送积分数量"
-                        min={0}
-                        max={100000}
-                    />
-                    <ProFormDigit
-                        name="signin_reward"
-                        label="签到奖励"
-                        tooltip="每日签到赠送积分数量"
-                        min={0}
-                        max={100000}
-                    />
-                </QueryFilter>
-            </div>
+            <Space direction="vertical" style={{
+                width: '100%'
+            }}
+            >
+                <div className={styles.config_form}>
+                    <h3>奖励激励</h3>
+                    <QueryFilter
+                        form={rewardForm}
+                        onFinish={async (values: any) => {
+                            putAdminConfig(values).then((res) => {
+                                if (res.code) {
+                                    message.error('保存失败')
+                                    return
+                                }
+                                message.success('保存成功')
+                                onGetConfig()
+                            })
+                        }}
+                        onReset={() => {
+                            onRewardFormSet(configs)
+                        }}
+                        size="large"
+                        collapsed={false}
+                        defaultCollapsed={false}
+                        requiredMark={false}
+                        defaultColsNumber={79}
+                        searchText="保存"
+                        resetText="恢复"
+                    >
+                        <ProFormDigit
+                            name="register_reward"
+                            label="注册奖励"
+                            tooltip="新用户注册赠送积分数量"
+                            min={0}
+                            max={100000}
+                        />
+                        <ProFormDigit
+                            name="signin_reward"
+                            label="签到奖励"
+                            tooltip="每日签到赠送积分数量"
+                            min={0}
+                            max={100000}
+                        />
+                    </QueryFilter>
+                </div>
+                <div className={styles.config_form}>
+                    <h3>历史记录</h3>
+                    <QueryFilter
+                        form={historyMessageForm}
+                        onFinish={onSave}
+                        onReset={() => {
+                            onRewardFormSet(configs)
+                        }}
+                        size="large"
+                        collapsed={false}
+                        defaultCollapsed={false}
+                        requiredMark={false}
+                        defaultColsNumber={79}
+                        searchText="保存"
+                        resetText="恢复"
+                    >
+                        <ProFormDigit
+                            name="history_message_count"
+                            label="携带数量"
+                            tooltip="新用户注册赠送积分数量"
+                            min={1}
+                            max={100000}
+                        />
+                    </QueryFilter>
+                </div>
+            </Space>
         </div>
     )
 }
